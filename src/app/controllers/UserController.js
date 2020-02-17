963c33d4ffef069e91902ba5c1de6554570f7848
Leandro Silva <lleandro.silva@outlook.com>
2020-02-17 17:59:30 -0300

    aula04

--- src/app/controllers/UserController.js
+++ src/app/controllers/UserController.js
@@ -17,5 +17,9 @@ class UserController {
       provider,
     });
   }
+
+  async update(req, res) {
+    return res.json({ ok: req.userId });
+  }
 }
 export default new UserController();
---
+++ src/config/auth.js
@@ -0,0 +1,4 @@
+export default {
+  secret: '4b3104244c416ae2e43162c2cd0a8556',
+  expiresIn: '7d',
+};
