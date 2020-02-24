1fb542233c7dea65ea932ab5ef71ea076d44ec49
Leandro Silva <lleandro.silva@outlook.com>
2020-02-24 17:46:29 -0300

    aula 06

--- src/app/controllers/FileController.js
+++ src/app/controllers/FileController.js
@@ -1,8 +1,13 @@
+import File from '../models/File';
+
 class FileController {
   async store(req, res) {
-    return res.json({
-      carreta: req.file.filename,
+    const { originalname: name, filename: path } = req.file;
+    const file = await File.create({
+      name,
+      path,
     });
+    return res.json(file);
   }
 }
 
